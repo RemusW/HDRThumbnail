ed9707f24132b78a0659ed22e855a73e570e6e8e
RemusW <rwong2100@hotmail.com>
2023-06-15 13:42:04 -0500

    more test

--- HDRThumbnail.Test/UnitTest1.cs
+++ HDRThumbnail.Test/UnitTest1.cs
@@ -69,6 +69,19 @@ namespace HDRThumbnail.Test
             Assert.AreEqual(image.Width, image.Height);
         }
 
+        [TestMethod]
+        public void VerticalImage()
+        {
+            string inputPath = MakeFilePath("hdr", "industrial_sunset_puresky_1k.hdr");
+            string outputPath = MakeFilePath("output", "VerticalImage.png");
+            int iwidth = 1024;
+            int iheight = 512;
+            int hFOV = 100;
+
+            HDRParser.createThumbnail(inputPath, outputPath, iwidth, iheight, hFOV);
+            Mat image = Cv2.ImRead(outputPath, ImreadModes.AnyColor | ImreadModes.AnyDepth);
+        }
+
         private string MakeFilePath(string baseFolderName, string fileName)
         {
             string projectRoot = Directory.GetParent(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)).FullName;
